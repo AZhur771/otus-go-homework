f4e4aad0e9dcd04ac2a9c02fb37b4123be000887
Журавков Андрей <zhr771@gmail.com>
2022-09-05 23:48:57 +0300

    Finish HW07

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
@@ -10,6 +14,51 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	if len(fromPath) == 0 || len(toPath) == 0 {
+		return ErrUnsupportedFile
+	}
+
+	file, err := os.Open(fromPath)
+	if err != nil {
+		return err
+	}
+
+	fileStat, err := file.Stat()
+	if err != nil {
+		return err
+	}
+
+	fileSize := fileStat.Size()
+	if fileSize < offset {
+		return ErrOffsetExceedsFileSize
+	}
+
+	if limit == 0 {
+		limit = fileSize
+	}
+
+	if fileSize < offset+limit {
+		limit = fileSize - offset
+	}
+
+	if offset != 0 {
+		file.Seek(offset, io.SeekStart)
+	}
+
+	copyFile, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+	defer copyFile.Close()
+
+	bar := pb.Start64(limit)
+	barReader := bar.NewProxyReader(file)
+	defer bar.Finish()
+
+	_, err = io.CopyN(copyFile, barReader, limit)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
